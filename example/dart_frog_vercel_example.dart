968dcb3378506597dea95adda29449859d5b08c7
Minsu Lee <amond@amond.net>
2023-01-30 12:49:17 +0900

    first commit

---
+++ example/dart_frog_vercel_example.dart
@@ -0,0 +1,6 @@
+import 'package:dart_frog_vercel/dart_frog_vercel.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/dart_frog_vercel.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dart_frog_vercel;
+
+export 'src/dart_frog_vercel_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
